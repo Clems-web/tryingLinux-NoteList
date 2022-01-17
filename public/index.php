fe6279c749bc727ef05655538fa391255623905b
clem <clement.servais1@gmail.com>
2022-01-17 12:34:39 +0100

    done

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../vendor/autoload.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/de3d4c2e3d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Notelist</title>
+</head>
+<body>
+    <div id="mainContainer">
+        <div id="noteContainer">
+                <textarea placeholder="votre texte ..."></textarea>
+        </div>
+    </div>
+</body>
+</html>
